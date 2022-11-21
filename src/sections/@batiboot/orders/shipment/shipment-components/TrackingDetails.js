7ad8f249f204ed3c6c8ee5da0311c9e10518a240
ZhyneAmenodin <amenodin.za28@s.msumain.edu.ph>
2022-11-21 14:47:21 +0800

    tracking modal

---
+++ src/sections/@batiboot/orders/shipment/shipment-components/TrackingDetails.js
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+// @mui
+import { Box, Stack, Button, Divider, Typography, InputAdornment, MenuItem, TextField, Grid } from '@mui/material';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+// utils
+import { fNumber } from '../../../../../utils/formatNumber';
+// components
+import Iconify from '../../../../../components/Iconify';
+import { RHFSelect, RHFTextField } from '../../../../../components/hook-form';
+
+// ----------------------------------------------------------------------
+const STATUS_OPTION = ['Preparing', 'Delivery in progress', 'Received', 'Not Delivered'];
+
+export default function TrackingDetails() {
+  const [trackingNum, setTrackingNum] = React.useState('2022001');
+  const [value, setValue] = React.useState(' ');
+  const [status, setStatus] = useState('Preparing');
+  const handleStatus = (event) => {
+    setStatus(event.target.value);
+  };
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography variant="h6" sx={{ color: 'text.disabled', mb: 3 }}>
+        Details:
+      </Typography>
+
+      <Stack spacing={3}>
+        <Stack alignItems="flex-end" spacing={1.5}>
+          <Stack direction={{ xs: 'column', md: 'row' }} sx={{ width: 1, pl: 2 }}>
+            <TextField
+              disabled
+              name="trackingNum"
+              label="Tracking Number"
+              value={trackingNum}
+              InputLabelProps={{ shrink: true }}
+            />
+          </Stack>
+          <Stack
+            direction={{ xs: 'column', md: 'row' }}
+            spacing={2}
+            justifyContent="flex-start"
+            sx={{ width: 1, p: 2 }}
+          >
+            {/* <TextField size="small" label="Order Recieve Date" InputLabelProps={{ shrink: true }} /> */}
+            <DesktopDatePicker
+              label="Order Recieve Date"
+              inputFormat="MM/dd/yyyy"
+              value={value}
+              onChange={handleChange}
+              renderInput={(params) => <TextField {...params} />}
+            />
+
+            {/* <TextField size="small" label="Status" InputLabelProps={{ shrink: true }} /> */}
+            <Grid sx={{ marginY: 2 }}>
+              <TextField
+                label="Tracking Status"
+                select
+                value={status}
+                onChange={handleStatus}
+                sx={{
+                  textTransform: 'capitalize',
+                }}
+              >
+                {STATUS_OPTION.map((option) => (
+                  <MenuItem
+                    key={option}
+                    value={option}
+                    sx={{
+                      mx: 1,
+                      my: 0.5,
+                      borderRadius: 0.75,
+                      typography: 'body2',
+                      textTransform: 'capitalize',
+                    }}
+                  >
+                    {option}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            {/* <RHFSelect
+                name={`items[${index}].service`}
+                label="Service type"
+                size="small"
+                InputLabelProps={{ shrink: true }}
+                SelectProps={{ native: false, sx: { textTransform: 'capitalize' } }}
+                sx={{ maxWidth: { md: 160 } }}
+              >
+                <MenuItem
+                  value=""
+                  sx={{
+                    mx: 1,
+                    borderRadius: 0.75,
+                    typography: 'body2',
+                    fontStyle: 'italic',
+                    color: 'text.secondary',
+                  }}
+                >
+                  None
+                </MenuItem>
+                <Divider />
+                {SERVICE_OPTIONS.map((option) => (
+                  <MenuItem
+                    key={option}
+                    value={option}
+                    sx={{
+                      mx: 1,
+                      my: 0.5,
+                      borderRadius: 0.75,
+                      typography: 'body2',
+                      textTransform: 'capitalize',
+                    }}
+                  >
+                    {option}
+                  </MenuItem>
+                ))}
+              </RHFSelect> */}
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
---
+++ src/sections/@batiboot/orders/shipment/shipment-components/TrackingLocation.js
@@ -0,0 +1,61 @@
+import PropTypes from 'prop-types';
+// form
+import React, { useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+// @mui
+import { Stack, Divider, Typography, Button, TextField } from '@mui/material';
+// hooks
+import useResponsive from '../../../../../hooks/useResponsive';
+import useToggle from '../../../../../hooks/useToggle';
+// _mock
+import { _invoiceAddressFrom, _invoiceAddressTo } from '../../../../../_mock/batiboot/invoice_mock/_invoice';
+// components
+import Iconify from '../../../../../components/Iconify';
+import TrackingNewAddress from './TrackingNewAddress';
+//
+
+// ----------------------------------------------------------------------
+
+export default function TrackingLocation() {
+  const upMd = useResponsive('up', 'md');
+
+  const { toggle: openFrom, onOpen: onOpenFrom, onClose: onCloseFrom } = useToggle();
+
+  const { toggle: openTo, onOpen: onOpenTo, onClose: onCloseTo } = useToggle();
+
+  const [addressFrom, setAddressFrom] = React.useState();
+  const [addressTo, SetAddressTo] = React.useState();
+
+  return (
+    <Stack
+      spacing={{ xs: 2, md: 5 }}
+      direction={{ xs: 'column', md: 'row' }}
+      divider={<Divider flexItem orientation={upMd ? 'vertical' : 'horizontal'} sx={{ borderStyle: 'dashed' }} />}
+      sx={{ p: 3 }}
+    >
+      <Stack sx={{ width: 1 }}>
+        <Stack direction="row" alignItems="center" justifyContent="space-evenly" sx={{ mb: 1 }}>
+          <Typography variant="h6" sx={{ color: 'text.disabled' }}>
+            Origin:
+          </Typography>
+          <TextField size="small" name="origin" InputLabelProps={{ shrink: true }} />
+
+          {/* <TrackingNewAddress open={openFrom} onClose={onCloseFrom} addressOptions={_invoiceAddressFrom} /> */}
+        </Stack>
+      </Stack>
+
+      <Stack sx={{ width: 1 }}>
+        <Stack direction="row" alignItems="center" justifyContent="space-evenly" sx={{ mb: 1 }}>
+          <Typography variant="h6" sx={{ color: 'text.disabled', mr: 1 }}>
+            Destination:
+          </Typography>
+          <TextField size="small" name="destination" InputLabelProps={{ shrink: true }} />
+
+          {/* <TrackingNewAddress open={openFrom} onClose={onCloseFrom} addressOptions={_invoiceAddressFrom} /> */}
+        </Stack>
+      </Stack>
+    </Stack>
+  );
+}
+
+// ----------------------------------------------------------------------
